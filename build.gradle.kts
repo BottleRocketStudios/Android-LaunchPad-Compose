0c77ad07474f434a08bdc681a2a0e79a91d0c367
Josh-Gebbeken <82959132+Josh-Gebbeken@users.noreply.github.com>
2022-07-26 13:40:13 -0600

    ASAA-12 Added detekt to Android LaunchPad Compose (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ buildscript {
 plugins {
     id(Config.ApplyPlugins.KT_LINT) version Config.KTLINT_GRADLE_VERSION
     id(Config.ApplyPlugins.MAVEN_PUBLISH)
+    id(Config.ApplyPlugins.DETEKT) version Config.DETEKT_VERSION
+}
+
+detekt {
+    config = files("$rootDir/config/detekt/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
 }
 
 allprojects {
@@ -25,4 +30,12 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true) // observe findings in your browser with structure and code snippets
+        xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+        txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
+    }
 }
